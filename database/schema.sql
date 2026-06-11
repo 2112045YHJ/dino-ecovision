57852cff61ad41f258c942bb648a86328fbcc6d6
steadykim11 <rlagksruf115@naver.com>
2026-06-11 16:43:22 +0900

    chore(sql): CHARACTER SET 중복 부분 제거

--- database/schema.sql
+++ database/schema.sql
@@ -4,7 +4,6 @@
 -- =======================================================
 
 SET NAMES utf8mb4;
-SET CHARACTER SET utf8mb4;
 
 CREATE DATABASE IF NOT EXISTS `dino_ecovision` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE `dino_ecovision`;
